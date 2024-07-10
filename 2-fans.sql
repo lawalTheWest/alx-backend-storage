d31e6db77a083a1c587386b1a8b74e5a517c449f
Tajudeen <lwltjdn@gmail.com>
2024-07-10 09:26:53 +0100

    ls

---
+++ 2-fans.sql
@@ -0,0 +1,6 @@
+-- An SQL script that ranks country origins of bands,
+-- Ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans desc;

e8db40ad44ec35060a83a6587290d2c665e2ba8c
lawalTheWest <lwltjdn@gmail.com>
2024-07-19 18:31:56 +0100

    updated the test file

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
+'''
+    Main file
+'''
 import redis
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
+data = b'hello'
 key = cache.store(data)
 print(key)
 
